7a3e207ca8724049277409218c1df594a87fb311
Utkarsh Srivastav <71812037+uttu2108@users.noreply.github.com>
2020-12-20 21:14:01 +0530

    Add files via upload

---
+++ us.js
@@ -0,0 +1,19 @@
+
+
+
+    function textingText(textone){
+
+function morningtxt (nameText){
+console.log(textone ,nameText())
+}
+return morningtxt
+
+    }
+
+    function getnameText() {
+        return document.getElementById('namebox').value
+        
+    }
+
+    let Greet= textingText('Good Morning')
+    let hru= textingText('How are you ')
